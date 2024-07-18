fc2ae9744a60baee1db3fd2cee4e757a19bb8fda
Samarjeet <samarjeetpandey1402@gmail.com>
2024-07-18 11:54:11 +0530

    resolve question

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="main">
+      <h1>Welcome to My React App</h1>
       <p>I am learning react. My life is getting better.</p>
     </div>
   );
